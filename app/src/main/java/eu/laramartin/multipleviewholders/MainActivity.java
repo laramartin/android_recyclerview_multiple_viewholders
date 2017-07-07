a90e6a24f21d73e1ed13431aba8e0f8e510f1904
Lara Martin <larikimc@gmail.com>
2017-07-07 19:04:36 +0200

    add empty RecyclerView and adapter

--- app/src/main/java/eu/laramartin/multipleviewholders/MainActivity.java
+++ app/src/main/java/eu/laramartin/multipleviewholders/MainActivity.java
@@ -2,6 +2,8 @@ package eu.laramartin.multipleviewholders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter());
     }
 }
